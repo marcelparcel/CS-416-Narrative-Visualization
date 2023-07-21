764c29cbe8a410ff1e9ef3b5d3c7d5ad13cb7025
Marcel Hoang <marcelh2@illinois.edu>
2023-07-21 10:53:11 -0500

    csv parsing

--- data.js
+++ data.js
@@ -1,5 +1,11 @@
 async function init() {
     const gg_data = await d3.csv("./datasets/gg_album_sales.csv");
     const bg_data = await d3.csv("./datasets/bg_album_sales.csv");
-    var tot_data = gg_data.concat(bg_data);
+    var tot_data = (await d3.csv("./datasets/gg_album_sales.csv")).concat((await d3.csv("./datasets/bg_album_sales.csv")));
+    tot_data.sort(function (x,y) {
+        return d3.descending(parseInt(x.sales), parseInt(y.sales));
+    });
+    for (var i = 1; i <= tot_data.length; i++) {
+        tot_data[i-1].rank = i;
+    }
 }
