1a758366062855e2b244a2704c07e95e81526e14
Marcel Hoang <marcelh2@illinois.edu>
2023-07-26 15:10:15 -0500

    sunburst graph

--- helper.js
+++ helper.js
@@ -1,14 +1,34 @@
 export class Story {
     #scenes = [];
-    curr_scene;
+    currScene;
     constructor() {
-        this.curr_scene = 0;
+        this.currScene = 0;
     }
-    get curr_scene() {
-        return this.curr_scene;
+    get currScene() {
+        return this.currScene;
     }
-    get curr_scene_cnt() {
-        return this.#scenes[this.curr_scene];
+    set currScene(i) {
+        if (i === this.currScene + 1 && this.currScene < this.#scenes.length - 1) {
+            this.currScene++;
+        } else if (i === this.currScene - 1 && this.currScene > 0) {
+            this.currScene--;
+        }
+    }
+    get currSceneContent() {
+        return this.#scenes[this.currScene];
+    }
+    get nextSceneContent() {
+        if (this.currScene < this.#scenes.length - 1) {
+            return this.#scenes[this.currScene + 1]
+        } else return this.#scenes[this.currScene];
+    }
+    get prevSceneContent() {
+        if (this.currScene > 0) {
+            return this.#scenes[this.currScene - 1]
+        } else return this.#scenes[this.currScene];
+    }
+    get length() {
+        return this.#scenes.length;
     }
     addScene(scene) {
         (this.#scenes).push(scene);
@@ -32,6 +52,11 @@ export async function init() {
     return {"gg": gg_data, "bg": bg_data, "tot": tot_data};
 }
 
+export const text_node = t => {
+    document.getElementById("svg-container").classList.add("intro");
+    return document.createTextNode(t);
+}
+
 export function cumsum_line(tot, gg, bg) {
     const width = 1000;
     const height = 700;
@@ -44,10 +69,10 @@ export function cumsum_line(tot, gg, bg) {
     const obj_array = a => a.map(([date, sales]) => ({date, sales}));
 
     gg.sort((x,y) => d3.ascending(x.release, y.release));
-    const gg_sum = obj_array(transpose([gg.map(d => d.release), d3.cumsum(gg.map(d => d.sales))]));
+    const gg_sum = obj_array(transpose([gg.map(d => d.release), d3.cumsum(gg.map(d => d.sales/1000000))]));
 
     bg.sort((x,y) => d3.ascending(x.release, y.release));
-    const bg_sum = obj_array(transpose([bg.map(d => d.release), d3.cumsum(bg.map(d => d.sales))]));
+    const bg_sum = obj_array(transpose([bg.map(d => d.release), d3.cumsum(bg.map(d => d.sales/1000000))]));
 
     const xs = d3.scaleUtc(d3.extent(tot, d => d.release), [marginLeft, width - marginRight]);
     const ys = d3.scaleLinear([0, d3.max(bg_sum, d => d.sales)], [height - marginBottom, marginTop]);
@@ -57,7 +82,8 @@ export function cumsum_line(tot, gg, bg) {
     const svg = d3.create("svg")
                 .attr("width", width)
                 .attr("height", height)
-                .attr("class", "intro");
+                .attr("class", "graph")
+                .attr("id", "graph");
 
     svg.append("g")
     .attr("transform", `translate(0,${height - marginBottom})`)
@@ -75,7 +101,7 @@ export function cumsum_line(tot, gg, bg) {
         .attr("y", 10)
         .attr("fill", "currentColor")
         .attr("text-anchor", "start")
-        .text("Total Sales"));
+        .text("Total Sales in Millions of Units"));
 
     svg.append("path")
       .attr("fill", "none")
@@ -89,25 +115,125 @@ export function cumsum_line(tot, gg, bg) {
       .attr("stroke-width", 1.5)
       .attr("d", line(bg_sum));
 
-    return svg;
+    return svg.node();
 }
 
-export function pie_chart(data) {
+export function sunburst(data) {
+    const width = 500;
+    const height = width;
+    const radius = width/6;
 
+    const salesByGroup = d3.group(data, d => d.label, d => d.sublabel, d => d.group, d => d.album);
+    const hierarchy = d3.hierarchy(salesByGroup).sum(d => d.sales).sort((x,y) => d3.descending(x.value, y.value));
+
+    const color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, hierarchy.children.length + 1));
+
+    console.log(hierarchy);
+
+    const root = d3.partition()
+        .size([2 * Math.PI, hierarchy.height + 1])(hierarchy);
+
+    root.each(d => d.current = d);
+
+    const arc = d3.arc()
+        .startAngle(d => d.x0)
+        .endAngle(d => d.x1)
+        .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
+        .padRadius(radius * 1.5)
+        .innerRadius(d => d.y0 * radius)
+        .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1));
+
+    const svg = d3.create("svg")
+      .attr("viewBox", [-width / 2, -height / 2, width, width])
+      .style("font", "4px Rubik, sans-serif");
+
+    const path = svg.append("g")
+        .selectAll("path")
+        .data(root.descendants().slice(1))
+        .join("path")
+        .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data[0]); })
+        .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
+        .attr("pointer-events", d => arcVisible(d.current) ? "auto" : "none")
+        .attr("d", d => arc(d.current));
+
+    path.filter(d => (d.height > 1))
+        .style("cursor", "pointer")
+        .on("click", clicked);
+
+    const format = d3.format(",d");
+
+    path.append("title")
+        .text(d => `${d.ancestors().map(d => d.data[0]).reverse().join("->")}\n${format(d.value).concat(" units sold")}\n${d3.format(".2%")(d.value/200442871.0)}`);
+      
+    const label = svg.append("g")
+        .attr("pointer-events", "none")
+        .attr("text-anchor", "middle")
+        .style("user-select", "none")
+        .selectAll("text")
+        .data(root.descendants().slice(1))
+        .join("text")
+        .attr("dy", "0.35em")
+        .attr("fill-opacity", d => +labelVisible(d.current))
+        .attr("transform", d => labelTransform(d.current))
+        .text(d => d.data[0]);
+      
+    const parent = svg.append("circle")
+        .datum(root)
+        .attr("r", radius)
+        .attr("fill", "none")
+        .attr("pointer-events", "all")
+        .on("click", clicked);
+    
+    function clicked(event, p) {
+        parent.datum(p.parent || root);
+        
+        root.each(d => d.target = {
+            x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+            x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+            y0: Math.max(0, d.y0 - p.depth),
+            y1: Math.max(0, d.y1 - p.depth)
+        });
+        const t = svg.transition().duration(750);
+
+        path.transition(t)
+            .tween("data", d => {
+            const i = d3.interpolate(d.current, d.target);
+            return t => d.current = i(t);
+            })
+        .filter(function(d) {
+            return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+        })
+            .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+            .attr("pointer-events", d => arcVisible(d.target) ? "auto" : "none") 
+
+            .attrTween("d", d => () => arc(d.current));
+
+        label.filter(function(d) {
+            return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+        }).transition(t)
+            .attr("fill-opacity", d => +labelVisible(d.target))
+            .attrTween("transform", d => () => labelTransform(d.current));
+    }
+  
+    function arcVisible(d) {
+        return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+    }
+
+    function labelVisible(d) {
+        return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+    }
+
+    function labelTransform(d) {
+        const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
+        const y = (d.y0 + d.y1) / 2 * radius;
+        return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+    }
+
+    return svg.node();
 }
 
 export function bar_chart(data) {
 
-}
-
-export function prev(Story) {
-    Story.curr_scene--;
-    return Story.curr_scene;
-}
-
-export function next(Story) {
-    Story.curr_scene++;
-    return Story.curr_scene;
 }
 
 /*
--- script.js
+++ script.js
@@ -2,9 +2,31 @@ import * as help from "./helper.js";
 
 const data = await help.init();
 
-console.log(data);
-
 const story = new help.Story();
+//story.addScene(help.text_node("test"));
 story.addScene(help.cumsum_line(data.tot, data.gg, data.bg));
-d3.select("body").select("#svg-container")
-    .attr();
+story.addScene(help.sunburst(data.tot));
+//console.log(help.sunburst(data.tot));
+
+document.getElementsByClassName("header")[0].onclick = () => { 
+    document.getElementById("svg-container").appendChild(story.currSceneContent);
+    document.getElementById("svg-container").appendChild(document.getElementById("buttons"));
+    document.getElementById("buttons").style.display = "block";
+    //console.log(document.getElementById("graph").childNodes);
+}
+
+document.getElementsByClassName("prev")[0].onclick = () => {
+    if (story.currScene > 0) {
+        story.currScene--;
+        document.getElementById("svg-container").replaceChild(story.currSceneContent, story.nextSceneContent);
+    }
+    //console.log(document.getElementById("svg-container").childNodes);
+}
+
+document.getElementsByClassName("next")[0].onclick = () => { 
+    if (story.currScene < story.length - 1) {
+    story.currScene++;
+    document.getElementById("svg-container").replaceChild(story.currSceneContent, story.prevSceneContent);
+    }
+    //console.log(document.getElementById("svg-container").childNodes);
+}
